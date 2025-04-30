c98520204ef98ea07e61dc91b13011886492da8d
Sergey <maestrox@bk.ru>
2025-04-30 20:40:00 +0300

    зменения в файле

---
+++ test1.py
@@ -0,0 +1,10 @@
+def my_decorator(func):
+    def wrapper(name):  # wrapper принимает name
+        print("До вызова")
+        func(name)       # func тоже получает name
+        print("После вызова")
+    return wrapper
+
+@my_decorator
+def greet(name):
+    print(f"Привет, {name}!")
